5681dbad22f98fe42ad52b651f2e36ea9b3a7a9c
Centeno <centeno@diegocenteno.com>
2021-10-25 16:57:53 -0300

    NO_STORY Add Pipefy integration

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,7 +7,7 @@
     if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
       return !(value.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null);
     } else {
-      return !(value == '');
+      return value.length > 3;
     }
   }
 
@@ -78,43 +78,81 @@
     return formData;
   }
 
+  function saveAgilecrm(data) {
+    var contact = {};
+    contact.email = data.email;
+    contact.first_name = data.name;
+    contact.company = data.organization;
+    contact.phone = data.phone;
+    contact.webnote = data.message;
+    contact.tags = "Website Contact";
+    _agile.create_contact(contact, {
+        success: onSuccess,
+        error: function () {
+          _agile.set_email(contact.email)
+          _agile.update_contact(contact, {
+            success: onSuccess,
+            error: onError
+          });
+        }
+    });
+  }
+
+  function savePipefy(data) {
+    var payload = `
+      mutation { 
+        createCard(
+          input: {
+            pipe_id: 301858572,
+            fields_attributes:[
+              { field_id: "nome", field_value: "${data.name}"},
+              { field_id:"email", field_value: "${data.email}"},
+              { field_id:"n_mero_de_telefone", field_value: "${data.phone}"},
+              { field_id:"nome_da_empresa", field_value: "${data.organization}"},
+              { field_id:"mensagem", field_value: "${data.message}"}
+            ]
+          }
+        )
+        { card {id} }
+      }
+    `
+    var options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjp7ImlkIjozMDE0MzMzOTAsImVtYWlsIjoiY2VudGVub0BiZW1wLmNvbS5iciIsImFwcGxpY2F0aW9uIjozMDAxMTkyMzd9fQ.seaN9fW5gwTX2zBDt1ttUx4bscrQ3IQ701juv7GXayO0t7kB58cT6eF_BE-o2dbRamGAaAR6OHhvKFpUwZPjtg',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query: payload })
+    }
+  
+    return fetch('https://api.pipefy.com/graphql', options)
+      .then(response => response.json())
+      .then(response => {
+        if (response.errors !== undefined) {
+          throw response.errors
+        }
+      })
+  }
+
+  function onSuccess() {
+    $('#gform').hide();
+    $('#success').attr('class', 'd-block');
+  }
+
+  function onError(data) {
+    $('#error').attr('class', 'd-block');
+    console.log("error", data);
+  }
 
   $('#gform').on("submit", function (event) {
     event.preventDefault();
 
-    if (formValid()) {
-      var form = event.target;
-      var data = getFormData(form);
+    if (!formValid()) return false;
 
-      var contact = {};
-      contact.email = data.email;
-      contact.first_name = data.name;
-      contact.company = data.organization;
-      contact.phone = data.phone;
-      contact.webnote = data.message;
-      contact.tags = "Website Contact";
-      _agile.create_contact(contact, {
-          success: onSuccess,
-          error: function () {
-            _agile.set_email(contact.email)
-            _agile.update_contact(contact, {
-              success: onSuccess,
-              error: onError
-            });
-          }
-      });
-    } else {
-      return false;
-    }
-
-    function onSuccess() {
-      // $.post(form.action, getFormData(form));
-      $('#gform').hide();
-      $('#success').attr('class', 'd-block');
-    }
-
-    function onError(data) {
-      console.log("error", data);
-    }
+    var data = getFormData(event.target);
+    
+    saveAgilecrm(data);
+    savePipefy(data); // .then(response => onSuccess()).catch(err => onError(err));
   });
 })(jQuery);
